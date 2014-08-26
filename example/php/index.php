ad5e7fca78422e6898b23fc845ea10b538cfd9ef
Adam Heim <jefffan24@gmail.com>
2014-08-26 09:25:41 -0400

    Update example file with new select that uses new mixWithCurrent option

--- example/php/index.php
+++ example/php/index.php
@@ -15,12 +15,13 @@ $data = json_decode($file_contents, true);
 	<title></title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/css/bootstrap.css"/>
 	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.5.4/bootstrap-select.css"/>
+    <style>h3 { text-align: center; } .bootstrap-select { width: 100% !important; }</style>
 </head>
 <body>
 <div class="container">
 	<div class="row">
-		<div class="col-xs-6">
-			<h3>Without Ajax-Bootstrap-Select</h3>
+		<div class="col-xs-4">
+			<h3>Without<br>Ajax-Bootstrap-Select</h3>
 			<select id="selectpicker" class="selectpicker" data-live-search="true">
 				<?php
 				foreach($data as $d){
@@ -30,12 +31,20 @@ $data = json_decode($file_contents, true);
 				?>
 			</select>
 		</div>
-		<div class="col-xs-6">
-			<h3>With Ajax-Bootstrap-Select</h3>
+
+		<div class="col-xs-4">
+			<h3>With<br>Ajax-Bootstrap-Select</h3>
 			<select id="ajax-select" class="selectpicker" data-live-search="true">
 				<option value="" data-hidden="true">Select and Begin Typing</option>
 			</select>
 		</div>
+
+        <div class="col-xs-4">
+            <h3>Multiple<br>Ajax-Bootstrap-Select</h3>
+            <select id="ajax-select-multiple" class="selectpicker" multiple data-live-search="true">
+                <option value="" data-hidden="true">Select and Begin Typing</option>
+            </select>
+        </div>
 	</div>
 </div>
 
@@ -43,33 +52,38 @@ $data = json_decode($file_contents, true);
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.js"></script>
 <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.js"></script>
 <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.5.4/bootstrap-select.js"></script>
-<script type="text/javascript" src="../../ajaxSelectPicker.min.js"></script>
+<script type="text/javascript" src="../../ajaxSelectPicker.js"></script>
 <script>
-	$('.selectpicker').selectpicker();
-	$('#ajax-select').ajaxSelectPicker({
-		ajaxResultsPreHook: function(data){
-			var arr = [];
-			if(data.length){
-				for(var i = 0; i < data.length; i++){
-					arr.push({
-						text: data[i].Name,
-						value: data[i].Email
-					});
-				}
-			}
+    var commenSettings = {
+        ajaxResultsPreHook: function(data){
+            var arr = [];
+            if(data.length){
+                for(var i = 0; i < data.length; i++){
+                    arr.push({
+                        text: data[i].Name,
+                        value: data[i].Email
+                    });
+                }
+            }
 
-			return arr;
-		},
-		ajaxSearchUrl: 'ajax.php',
-		ajaxOptions: {
-			type: 'POST',
-			dataType: 'json',
-			data: {
-				q: '{{{q}}}' //We use {{{q}}} as a placeholder to specific I want the value from the textbox used
-			}
-		},
-		placeHolderOption: 'Select and Begin Typing'
-	});
+            return arr;
+        },
+        ajaxSearchUrl: 'ajax.php',
+        ajaxOptions: {
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                q: '{{{q}}}' //We use {{{q}}} as a placeholder to specific I want the value from the textbox used
+            }
+        },
+        placeHolderOption: 'Select and Begin Typing'
+    };
+
+	$('.selectpicker').selectpicker();
+	$('#ajax-select').ajaxSelectPicker(commenSettings);
+
+    var multipleSettings = $.extend({}, commenSettings, {mixWithCurrents: true});
+    $('#ajax-select-multiple').ajaxSelectPicker(multipleSettings)
 </script>
 </body>
 </html>

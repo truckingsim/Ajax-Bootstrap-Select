c1010738d4beda43deb0b0a281c249381ee90861
Adam Heim <jefffan24@gmail.com>
2014-10-27 12:51:44 -0400

    Added test jsonp.js file for recreating issues

---
+++ example/dataset.jsonp.js
@@ -0,0 +1,404 @@
+truckingsimABS (
+[
+{
+"Name": "Lacy",
+"Email": "ipsum@Quisqueac.net"
+},
+{
+"Name": "Chancellor",
+"Email": "neque.venenatis.lacus@neque.com"
+},
+{
+"Name": "Conan",
+"Email": "quis@id.com"
+},
+{
+"Name": "Igor",
+"Email": "habitant.morbi@mi.co.uk"
+},
+{
+"Name": "Mariam",
+"Email": "dolor.quam.elementum@interdumenimnon.edu"
+},
+{
+"Name": "Chancellor",
+"Email": "vestibulum@NullamnislMaecenas.org"
+},
+{
+"Name": "Quyn",
+"Email": "convallis.dolor.Quisque@Maecenas.ca"
+},
+{
+"Name": "Lance",
+"Email": "risus.In.mi@maurissagittis.edu"
+},
+{
+"Name": "Scarlett",
+"Email": "at@Integeraliquam.net"
+},
+{
+"Name": "Sandra",
+"Email": "nunc.sit@acmattis.net"
+},
+{
+"Name": "Chanda",
+"Email": "velit.Sed@nislMaecenas.com"
+},
+{
+"Name": "Jordan",
+"Email": "mi.lorem.vehicula@vitaeerat.com"
+},
+{
+"Name": "Emi",
+"Email": "nunc@aliquetPhasellusfermentum.net"
+},
+{
+"Name": "Yolanda",
+"Email": "vitae.sodales.nisi@diam.com"
+},
+{
+"Name": "Kai",
+"Email": "Cras.pellentesque@metusInnec.org"
+},
+{
+"Name": "Ulysses",
+"Email": "metus.eu.erat@Donec.ca"
+},
+{
+"Name": "Hedwig",
+"Email": "magna.a@elementum.edu"
+},
+{
+"Name": "Claudia",
+"Email": "enim@nonquam.net"
+},
+{
+"Name": "Myles",
+"Email": "enim.mi@sollicitudina.co.uk"
+},
+{
+"Name": "Michael",
+"Email": "id.magna.et@convallisligula.edu"
+},
+{
+"Name": "Mira",
+"Email": "ligula@pretiumnequeMorbi.co.uk"
+},
+{
+"Name": "Cara",
+"Email": "imperdiet@magna.ca"
+},
+{
+"Name": "Graiden",
+"Email": "vel@Praesenteunulla.org"
+},
+{
+"Name": "Kerry",
+"Email": "sem.mollis@arcuVivamussit.ca"
+},
+{
+"Name": "Shelley",
+"Email": "Nulla@ametluctus.com"
+},
+{
+"Name": "Nathan",
+"Email": "malesuada.id@sodalesat.org"
+},
+{
+"Name": "Phillip",
+"Email": "auctor@nonsapienmolestie.org"
+},
+{
+"Name": "Maggie",
+"Email": "scelerisque.dui@laoreet.ca"
+},
+{
+"Name": "August",
+"Email": "dolor@Cras.net"
+},
+{
+"Name": "Maile",
+"Email": "molestie@metusInlorem.edu"
+},
+{
+"Name": "Eden",
+"Email": "elit.dictum@Fuscedolorquam.co.uk"
+},
+{
+"Name": "Darryl",
+"Email": "pretium.neque.Morbi@Inornare.ca"
+},
+{
+"Name": "Davis",
+"Email": "Ut@nectempus.com"
+},
+{
+"Name": "Mechelle",
+"Email": "tellus.faucibus@arcu.org"
+},
+{
+"Name": "Dean",
+"Email": "semper.egestas@sagittis.org"
+},
+{
+"Name": "Olympia",
+"Email": "per.inceptos@interdumCurabitur.edu"
+},
+{
+"Name": "Uriel",
+"Email": "est.ac.facilisis@primisinfaucibus.org"
+},
+{
+"Name": "Barry",
+"Email": "dui@cursusin.co.uk"
+},
+{
+"Name": "Zenaida",
+"Email": "erat.volutpat@penatibuset.com"
+},
+{
+"Name": "Celeste",
+"Email": "Nunc.laoreet@sollicitudinadipiscingligula.ca"
+},
+{
+"Name": "Nerea",
+"Email": "eros.nec@maurisSuspendissealiquet.ca"
+},
+{
+"Name": "Dorothy",
+"Email": "In@Donec.net"
+},
+{
+"Name": "Ramona",
+"Email": "lacus.pede.sagittis@massaIntegervitae.ca"
+},
+{
+"Name": "Maite",
+"Email": "interdum.libero.dui@aliquetPhasellusfermentum.com"
+},
+{
+"Name": "Keith",
+"Email": "Etiam@necmollis.com"
+},
+{
+"Name": "Mark",
+"Email": "enim@musProin.co.uk"
+},
+{
+"Name": "Neil",
+"Email": "diam.at.pretium@Quisquelibero.net"
+},
+{
+"Name": "Ivana",
+"Email": "dignissim.magna.a@aliquameu.org"
+},
+{
+"Name": "Arden",
+"Email": "sed.leo.Cras@interdumCurabiturdictum.edu"
+},
+{
+"Name": "Inez",
+"Email": "non@enimNunc.com"
+},
+{
+"Name": "Lenore",
+"Email": "mauris@elementumlorem.com"
+},
+{
+"Name": "Darius",
+"Email": "Maecenas.iaculis.aliquet@Aliquamultrices.co.uk"
+},
+{
+"Name": "Natalie",
+"Email": "Proin@euduiCum.edu"
+},
+{
+"Name": "Aidan",
+"Email": "auctor@nibhlaciniaorci.net"
+},
+{
+"Name": "Logan",
+"Email": "mollis@vitaenibh.co.uk"
+},
+{
+"Name": "Raymond",
+"Email": "lacus@inconsectetuer.net"
+},
+{
+"Name": "Gregory",
+"Email": "Integer@egestas.edu"
+},
+{
+"Name": "Adena",
+"Email": "adipiscing.Mauris.molestie@pharetraNamac.net"
+},
+{
+"Name": "Nash",
+"Email": "elementum.purus@musAenean.com"
+},
+{
+"Name": "Wanda",
+"Email": "aliquet.molestie@famesac.co.uk"
+},
+{
+"Name": "Quail",
+"Email": "felis@ornare.co.uk"
+},
+{
+"Name": "Martin",
+"Email": "sem.Pellentesque@Duis.co.uk"
+},
+{
+"Name": "Xandra",
+"Email": "at.auctor.ullamcorper@milacinia.com"
+},
+{
+"Name": "Alfreda",
+"Email": "scelerisque@Sed.net"
+},
+{
+"Name": "Ralph",
+"Email": "sodales.elit.erat@nasceturridiculusmus.edu"
+},
+{
+"Name": "Blake",
+"Email": "magna@Cum.net"
+},
+{
+"Name": "Libby",
+"Email": "augue.malesuada@tincidunt.net"
+},
+{
+"Name": "Desirae",
+"Email": "rhoncus.id.mollis@ipsumPhasellus.org"
+},
+{
+"Name": "Zephania",
+"Email": "nunc.interdum@tincidunt.org"
+},
+{
+"Name": "Alexa",
+"Email": "Sed.id@sodalesnisi.net"
+},
+{
+"Name": "Roary",
+"Email": "sed.consequat@etultrices.edu"
+},
+{
+"Name": "Brynn",
+"Email": "montes@sitametdapibus.net"
+},
+{
+"Name": "Carl",
+"Email": "ipsum.Phasellus@loremsit.com"
+},
+{
+"Name": "Uta",
+"Email": "Curabitur.vel.lectus@facilisis.edu"
+},
+{
+"Name": "Lee",
+"Email": "ligula.tortor.dictum@Nunc.edu"
+},
+{
+"Name": "Channing",
+"Email": "pellentesque.Sed.dictum@orci.org"
+},
+{
+"Name": "Asher",
+"Email": "blandit@id.com"
+},
+{
+"Name": "Medge",
+"Email": "vitae.diam@bibendumullamcorperDuis.org"
+},
+{
+"Name": "Nomlanga",
+"Email": "tristique@consequatenim.ca"
+},
+{
+"Name": "Amity",
+"Email": "Integer@cursusa.edu"
+},
+{
+"Name": "Rebekah",
+"Email": "faucibus.leo.in@rutrumFuscedolor.ca"
+},
+{
+"Name": "Cleo",
+"Email": "Praesent@nostra.com"
+},
+{
+"Name": "Candice",
+"Email": "erat.Vivamus.nisi@venenatisamagna.com"
+},
+{
+"Name": "Sade",
+"Email": "Aliquam@auctornuncnulla.ca"
+},
+{
+"Name": "Jared",
+"Email": "bibendum.Donec@turpisegestasAliquam.ca"
+},
+{
+"Name": "Erin",
+"Email": "urna.Nunc.quis@elitfermentum.net"
+},
+{
+"Name": "William",
+"Email": "Integer@eratnonummyultricies.org"
+},
+{
+"Name": "Dora",
+"Email": "quis@afelisullamcorper.co.uk"
+},
+{
+"Name": "Chaim",
+"Email": "nisi.Mauris.nulla@orciconsectetuereuismod.com"
+},
+{
+"Name": "Hayes",
+"Email": "cubilia@IntegermollisInteger.org"
+},
+{
+"Name": "Morgan",
+"Email": "Aliquam@interdum.org"
+},
+{
+"Name": "Ignatius",
+"Email": "ut.sem.Nulla@duiCraspellentesque.org"
+},
+{
+"Name": "Madeline",
+"Email": "ipsum.ac.mi@adipiscingnon.co.uk"
+},
+{
+"Name": "Joshua",
+"Email": "neque.In.ornare@auctorveliteget.edu"
+},
+{
+"Name": "Fulton",
+"Email": "faucibus.orci.luctus@lorem.org"
+},
+{
+"Name": "Elizabeth",
+"Email": "tempus@nibhPhasellusnulla.edu"
+},
+{
+"Name": "Elmo",
+"Email": "penatibus.et@diam.com"
+},
+{
+"Name": "Hammett",
+"Email": "pharetra.felis.eget@convallisest.ca"
+},
+{
+"Name": "Quyn",
+"Email": "tellus.Suspendisse.sed@duiCumsociis.edu"
+},
+{
+"Name": "Maisie",
+"Email": "mauris.eu@Naminterdum.co.uk"
+}
+]
+)

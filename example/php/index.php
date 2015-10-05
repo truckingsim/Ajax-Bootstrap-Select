2e4dfa5c90b7879b9f4817793317c21e77d7ee3f
Adam Heim <jefffan24@gmail.com>
2015-10-05 13:27:14 -0400

    Reforamt example php for consistent styling and add new example for previously selected options

--- example/php/index.php
+++ example/php/index.php
@@ -1,8 +1,8 @@
 <?php
 $file_contents = file_get_contents('../dataset.json');
 
-if(!$file_contents){
-	throw new Exception('Invalid file name');
+if (!$file_contents) {
+    throw new Exception('Invalid file name');
 }
 
 $data = json_decode($file_contents, true);
@@ -11,69 +11,87 @@ $data = json_decode($file_contents, true);
 <!doctype html>
 <html lang="en-US">
 <head>
-	<meta charset="UTF-8">
-	<title></title>
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/css/bootstrap.min.css"/>
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css"/>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css"/>
     <link rel="stylesheet" href="../../dist/css/ajax-bootstrap-select.css"/>
-    <style>h3 { text-align: center; } .bootstrap-select { width: 100% !important; }</style>
+    <style>h3 {
+            text-align: center;
+        }
+
+        .bootstrap-select {
+            width: 100% !important;
+        }</style>
 </head>
 <body>
 <div class="container">
-	<div class="row">
-		<div class="col-xs-4">
-			<h3>Without<br>Ajax-Bootstrap-Select</h3>
-			<select id="selectpicker" class="selectpicker" data-live-search="true">
-				<?php
-				foreach($data as $d){
-					echo '<option value="' . $d['Email'] . '">' . $d['Name'] . '</option>';
-				}
+    <div class="row">
+        <div class="col-xs-4">
+            <h3>Without<br>Ajax-Bootstrap-Select</h3>
+            <select id="selectpicker" class="selectpicker" data-live-search="true">
+                <?php
+                foreach ($data as $d) {
+                    echo '<option value="' . $d['Email'] . '">' . $d['Name'] . '</option>';
+                }
 
-				?>
-			</select>
-		</div>
+                ?>
+            </select>
+        </div>
 
-		<div class="col-xs-4">
-			<h3>With<br>Ajax-Bootstrap-Select</h3>
-			<select id="ajax-select" class="selectpicker with-ajax" data-live-search="true"></select>
-		</div>
+        <div class="col-xs-4">
+            <h3>With<br>Ajax-Bootstrap-Select</h3>
+            <select id="ajax-select" class="selectpicker with-ajax" data-live-search="true"></select>
+        </div>
 
         <div class="col-xs-4">
             <h3>Multiple<br>Ajax-Bootstrap-Select</h3>
             <select id="ajax-select-multiple" class="selectpicker with-ajax" multiple data-live-search="true"></select>
         </div>
-	</div>
+    </div>
+    <div class="row">
+        <div class="col-xs-4">
+            <h3>Cached Options<br>Ajax-Bootstrap-Select</h3>
+            <select class="selectpicker with-ajax" data-live-search="true" multiple>
+                <option value="neque.venenatis.lacus@neque.com" data-subtext="neque.venenatis.lacus@neque.com" selected>
+                    Chancellor
+                </option>
+            </select>
+        </div>
+    </div>
 </div>
 
 
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
+<script type="text/javascript"
+        src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script type="text/javascript"
+        src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
 <script type="text/javascript" src="../../dist/js/ajax-bootstrap-select.js"></script>
 <script>
     var options = {
-        ajax: {
-            url: 'ajax.php',
-            type: 'POST',
+        ajax          : {
+            url     : 'ajax.php',
+            type    : 'POST',
             dataType: 'json',
             // Use "{{{q}}}" as a placeholder and Ajax Bootstrap Select will
             // automatically replace it with the value of the search query.
-            data: {
+            data    : {
                 q: '{{{q}}}'
             }
         },
-        locale: {
+        locale        : {
             emptyTitle: 'Select and Begin Typing'
         },
-        log: 3,
+        log           : 3,
         preprocessData: function (data) {
             var i, l = data.length, array = [];
             if (l) {
-                for(i = 0; i < l; i++){
+                for (i = 0; i < l; i++) {
                     array.push($.extend(true, data[i], {
-                        text: data[i].Name,
+                        text : data[i].Name,
                         value: data[i].Email,
-                        data: {
+                        data : {
                             subtext: data[i].Email
                         }
                     }));
@@ -85,7 +103,8 @@ $data = json_decode($file_contents, true);
         }
     };
 
-	$('.selectpicker').selectpicker().filter('.with-ajax').ajaxSelectPicker(options);
+    $('.selectpicker').selectpicker().filter('.with-ajax').ajaxSelectPicker(options);
+    $('select').trigger('change');
 </script>
 </body>
 </html>

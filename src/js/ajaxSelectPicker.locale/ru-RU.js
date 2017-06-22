57931d737db3c46ae55bbe9c9738ff49587fc1dd
bercut <bercut>
2017-06-23 00:38:53 +0500

    add russian locale

---
+++ src/js/ajaxSelectPicker.locale/ru-RU.js
@@ -0,0 +1,77 @@
+/*
+ * Note: You do not have to load this translation file. English is the
+ * default language of this plugin and is compiled into it automatically.
+ *
+ * This file is just to serve as the default string mappings and as a
+ * template for future translations.
+ * @see: ./src/js/locale/en-US.js
+ *
+ * Four character language codes are supported ("en-US") and will always
+ * take precedence over two character language codes ("en") if present.
+ *
+ * When copying this file, remove all comments except the one above the
+ * definition objection giving credit to the translation author.
+ */
+
+/*!
+ * English translation for the "en-US" and "en" language codes.
+ * Mark Carver <mark.carver@me.com>
+ */
+$.fn.ajaxSelectPicker.locale['ru-RU'] = {
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} currentlySelected = 'Currently Selected'
+     * @markdown
+     * The text to use for the label of the option group when currently selected options are preserved.
+     */
+    currentlySelected: 'Выбрано',
+
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} emptyTitle = 'Select and begin typing'
+     * @markdown
+     * The text to use as the title for the select element when there are no items to display.
+     */
+    emptyTitle: 'Выделите и начните печатать',
+
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} errorText = ''Unable to retrieve results'
+     * @markdown
+     * The text to use in the status container when a request returns with an error.
+     */
+    errorText: 'Невозможно получить результат',
+
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} searchPlaceholder = 'Search...'
+     * @markdown
+     * The text to use for the search input placeholder attribute.
+     */
+    searchPlaceholder: 'Искать...',
+
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} statusInitialized = 'Start typing a search query'
+     * @markdown
+     * The text used in the status container when it is initialized.
+     */
+    statusInitialized: 'Начните печатать запрос для поиска',
+
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} statusNoResults = 'No Results'
+     * @markdown
+     * The text used in the status container when the request returns no results.
+     */
+    statusNoResults: 'Нет результатов',
+
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} statusSearching = 'Searching...'
+     * @markdown
+     * The text to use in the status container when a request is being initiated.
+     */
+    statusSearching: 'Поиск...'
+};
+$.fn.ajaxSelectPicker.locale.ru = $.fn.ajaxSelectPicker.locale['ru-RU'];
